7224ebeb421df01971c4edeaa4e2b25f258419fc
lance-ericson <donald.romeo1100@outlook.com>
2026-05-29 09:00:06 -0400

    Serve static assets from dist

---
+++ dist/server/assets/_tanstack-start-manifest_v-DQQNjJDo.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "E:/data/MY DATA/job_profile_richard/new/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CuHlwt1g.js"] }, "/": { filePath: "E:/data/MY DATA/job_profile_richard/new/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CcMNeQCn.js"] } }, clientEntry: "/assets/index-CuHlwt1g.js" });
+export {
+  tsrStartManifest
+};
